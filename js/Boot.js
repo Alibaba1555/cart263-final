f42abdfc3e7f1530c5a69f7f762b27cc746b779d
Alibaba1555 <tianshunwus@gmail.com>
2025-04-15 14:18:12 -0400

    test

--- js/Boot.js
+++ js/Boot.js
@@ -6,79 +6,78 @@ class Boot extends Phaser.Scene {
     }
 
     preload() {
-
         //load Player Assets
-        this.load.spritesheet("avatar-idle", "Assets/Character/Player/_Idle.png", {
+        this.load.spritesheet("avatar-idle", "Assets/Player/_Idle.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 9
         });
 
-        this.load.spritesheet("avatar-run", "Assets/Character/Player/_Run.png", {
+        this.load.spritesheet("avatar-run", "Assets/Player/_Run.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 9
         });
 
-        this.load.spritesheet("attack1", "Assets/Character/Player/_Attack.png", {
+        this.load.spritesheet("attack1", "Assets/Player/_Attack.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 3
         });
 
-        this.load.spritesheet("attack2", "Assets/Character/Player/_Attack2.png", {
+        this.load.spritesheet("attack2", "Assets/Player/_Attack2.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 5
         });
 
-        this.load.spritesheet("roll", "Assets/Character/Player/_Roll.png", {
+        this.load.spritesheet("roll", "Assets/Player/_Roll.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 11
         });
 
-        this.load.spritesheet("avatar-jump", "Assets/Character/Player/_Jump.png", {
+        this.load.spritesheet("avatar-jump", "Assets/Player/_Jump.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 2
         });
 
-        this.load.spritesheet("avatar-fall", "Assets/Character/Player/_Fall.png", {
+        this.load.spritesheet("avatar-fall", "Assets/Player/_Fall.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 2
         });
 
-        this.load.spritesheet("avatar-Jumpfall", "Assets/Character/Player/_JumpFall.png", {
+        this.load.spritesheet("avatar-Jumpfall", "Assets/Player/_JumpFall.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 1
         });
 
-        this.load.spritesheet("avatar-wallslide", "Assets/Character/Player/_WallSlide.png", {
+        this.load.spritesheet("avatar-wallslide", "Assets/Player/_WallSlide.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 2
         });
 
-        this.load.spritesheet("avatar-crouchDown", "Assets/Character/Player/_CrouchAll.png", {
+        this.load.spritesheet("avatar-crouchDown", "Assets/Player/_CrouchAll.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 1
         });
 
-        this.load.spritesheet("avatar-up", "Assets/Character/Player/_CrouchAll.png", {
+        this.load.spritesheet("avatar-up", "Assets/Player/_CrouchAll.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
@@ -86,35 +85,30 @@ class Boot extends Phaser.Scene {
             endFrame: 2
         });
 
-        this.load.spritesheet("avatar-jump2", "Assets/Character/Player/_Jump2.png", {
+        this.load.spritesheet("avatar-jump2", "Assets/Player/_Jump2.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 11
         });
 
-
-        this.load.spritesheet("avatar-death", "Assets/Character/Player/_Death.png", {
+        this.load.spritesheet("avatar-death", "Assets/Player/_Death.png", {
             frameWidth: 120,
             frameHeight: 80,
             spacing: 0,
             endFrame: 9
         });
 
-        this.load.spritesheet("wind1", "Assets/Character/Player/wind.png", {
+        this.load.spritesheet("wind1", "Assets/Player/wind.png", {
             frameWidth: 64,
             frameHeight: 64,
             spacing: 0,
             endFrame: 12
         });
 
-        this.load.image("avatar-takesHit", "Assets/Character/Player/_Hit.png");
+        this.load.image("avatar-takesHit", "Assets/Player/_Hit.png");
 
-        /**
-         * Load enemy
-         */
-
-        //Boar
+        // Enemy
         this.load.spritesheet("Boar-idle", "Assets/enemy/Boar_normal/Idle-Sheet.png", {
             frameWidth: 48,
             frameHeight: 32,
@@ -136,7 +130,6 @@ class Boot extends Phaser.Scene {
             endFrame: 5
         });
 
-        //mushroom 
         this.load.spritesheet("Mushroom-run", "Assets/enemy/Mushroom/Run.png", {
             frameWidth: 150,
             frameHeight: 39,
@@ -147,40 +140,35 @@ class Boot extends Phaser.Scene {
             frameWidth: 150,
             frameHeight: 39,
             endFrame: 3
-        })
+        });
 
-
-        //load map tile
+        // Map and tiles
         this.load.tilemapTiledJSON('level_1', "Assets/Map/Level1.json");
-        this.load.tilemapTiledJSON('level_2', "Assets/Map/Level2.json")
+        this.load.tilemapTiledJSON('level_2', "Assets/Map/Level2.json");
         this.load.image('tiles-1', 'Assets/Tiles/ForestTiles.png');
         this.load.image('tiles-2', 'Assets/Tiles/ForestTiles2.png');
+        this.load.image("Test-tile", "Assets/Tiles/Forest/Tiles.png");
 
-        //load bonfire 
+        // Bonfire and portal
         this.load.image('bonfire', "Assets/bonfire/bonfire.png");
-
-        //load portal 
         this.load.spritesheet("portal-1", "Assets/Portal/Portal_1.png", {
             frameWidth: 32,
             frameHeight: 32,
             endFrame: 2
         });
-
         this.load.spritesheet("portal-2", "Assets/Portal/Portal_2.png", {
             frameWidth: 32,
             frameHeight: 32,
             endFrame: 2
         });
 
-        this.load.image("Test-tile", "Assets/Tiles/Forest/Tiles.png");
-
-        //load collectables 
+        // Collectables
         this.load.image('redDiamond', "Assets/collectables/red_diamond.png");
         this.load.image('blueDiamond', "Assets/collectables/blue_diamond.png");
         this.load.image('redPotion', "Assets/collectables/red_potion.png");
         this.load.image('bluePotion', "Assets/collectables/blue_potion.png");
 
-        //load player projectile 
+        // Projectiles
         this.load.spritesheet("player_Projectile", "Assets/Effect/Projectile/Player_projectile1.png", {
             frameWidth: 32,
             frameHeight: 31,
@@ -188,15 +176,12 @@ class Boot extends Phaser.Scene {
             endFrame: 3
         });
 
-        //load enemy projectile 
         this.load.spritesheet("mushroom_Projectile", "Assets/Effect/Projectile/Mushroom_Projectile.png", {
             frameWidth: 50,
             frameHeight: 50,
-            endFrame: 7,
-        })
+            endFrame: 7
+        });
 
-
-        //load enemy dead Effect 
         this.load.spritesheet("enemy_dead", "Assets/Effect/Enemy_Dead/Enemies_dead.png", {
             frameWidth: 32,
             frameHeight: 32,
@@ -204,16 +189,11 @@ class Boot extends Phaser.Scene {
             endFrame: 3
         });
 
-        //load attack box sprite(empty)
+        // Attack box and background
         this.load.image("attack-box", "Assets/AttackBox/attackBox_Sprite.png");
-
-        //load background
         this.load.image("forest-bg", "Assets/Map/background forest.png");
 
-
-
-
-        //audio preload
+        // Audio
         this.load.audio("swingsword", "Assets/Audio/swingsword.mp3");
         this.load.audio("flying-attack-enemy", "Assets/Audio/Flying Attack_Enemy.mp3");
         this.load.audio("heal", "Assets/Audio/heal.mp3");
@@ -226,7 +206,6 @@ class Boot extends Phaser.Scene {
         this.load.audio('bgm-forest', 'Assets/Music/forest.mp3');
         this.load.audio('pickup', "Assets/Audio/coin_pickup.wav");
         this.load.audio('stepSound', "Assets/Audio/step_mud.wav");
-
 
         this.load.on('complete', () => {
             this.startGame();
